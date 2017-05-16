ef0407f2182f68efeb13d00a58e91b9bbee1ce40
cbreeden <github@u.breeden.cc>
2017-05-16 00:06:42 -0500

    continue refactor

--- src/font.rs
+++ src/font.rs
@@ -11,66 +11,65 @@ pub enum Version {
     TrueType,
 }
 
+
 static_size!(Version = 4);
-
-impl Table for Version {
-    fn parse(buf: &[u8]) -> Result<(&[u8], Version)> {
+versioned_table!(Version,
+    Tag => |tag| {
         const VERSION1: [u8; 4] = [0x00, 0x01, 0x00, 0x00];
-
-        if buf.len() < Self::static_size() {
-            return Err(Error::UnexpectedEof)
-        }
-
-        let (buf, tag) = Tag::parse(buf)?;
-        let ver = match &tag.0 {
+        match &tag.0 {
             b"OTTO" => Version::OpenType,
             &VERSION1 | b"true" | b"typ1" => Version::TrueType,
             b"ttcf" => return Err(Error::TtcfUnsupported),
             _ => return Err(Error::InvalidData),
-        };
-
-        Ok((buf, ver))
+        }
     }
+);
+
+#[derive(Table, Debug)]
+struct OffsetTable {
+    sfnt_version: Version,
+    num_tables: u16,
+    search_range: u16,
+    entry_selector: u16,
+    range_shift: u16,
 }
 
-#[derive(Debug, Table)]
-struct OffsetTable {
+#[derive(Table, Debug)]
+struct TableRecord {
     tag:       Tag,
     check_sum: u32,
     offset:    Offset32,
     length:    u32,
 }
 
-#[derive(Debug, Table)]
+#[derive(Debug)]
 struct Font<'a> {
     buf: &'a [u8],
     version: Version,
     num_tables: u16,
-    search_range: Reserved<u16>,
-    entry_selector: Reserved<u16>,
-    range_shirt: Reserved<u16>,
-    // search_range:   u16,
-    // entry_selector: u16,
-    // range_shift:    u16
 }
 
-impl<'a> Font<'a> {
-    fn table_iter<'b> (&'b self) -> TableIter<'b> {
+impl<'f> Font<'f> {
+    fn from_buffer<'b: 'f>(buf: &'b [u8]) -> Result<Font<'f>> {
+        if buf.len() < OffsetTable::static_size() {
+            return Err(Error::InvalidData)
+        }
+
+        let (_, offset_table) = OffsetTable::parse(buf)?;
+
+        Ok(Font {
+            buf: buf,
+            num_tables: offset_table.num_tables,
+            version: offset_table.sfnt_version,
+        })
+    }
+
+    fn tables(&self) -> TableIter {
             TableIter {
                 buf: self.buf,
                 pos: 0,
                 max: self.num_tables as usize,
             }
-    }
-
-    fn get_table_offset(&self, tag: Tag, buf: &[u8]) -> Option<u32> {
-        for table in self.table_iter() {
-            let tbl = table.unwrap();
-            if tbl.tag == tag {
-                return Some(tbl.offset.0)
-            }
-        }
-        None
     }
 }
 
@@ -81,14 +80,14 @@ struct TableIter<'a> {
 }
 
 impl<'a> Iterator for TableIter<'a> {
-    type Item = Result<OffsetTable>;
+    type Item = Result<TableRecord>;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.pos >= self.max {
             return None
         }
 
-        let next = match OffsetTable::parse(self.buf) {
+        let next = match TableRecord::parse(self.buf) {
             Err(e) => return Some(Err(e)),
             Ok(n)  => n,
         };
@@ -100,41 +99,41 @@ impl<'a> Iterator for TableIter<'a> {
     }
 }
 
-#[cfg(test)]
-mod test {
-    use super::Font;
-    use super::Table;
-    use super::hhea;
-    use ::decode::primitives::Tag;
+// #[cfg(test)]
+// mod test {
+//     use super::Font;
+//     use super::Table;
+//     use super::hhea;
+//     use ::decode::primitives::Tag;
 
-    #[test]
-    fn print_tables() {
-        use std::fs::File;
-        use std::io::BufReader;
-        use std::io::prelude::*;
+//     #[test]
+//     fn print_tables() {
+//         use std::fs::File;
+//         use std::io::BufReader;
+//         use std::io::prelude::*;
 
-        let file = File::open(r"data/OpenSans-Regular.ttf")
-            .expect("Unable to open file");
+//         let file = File::open(r"data/OpenSans-Regular.ttf")
+//             .expect("Unable to open file");
 
-        let mut reader = BufReader::new(file);
-        let mut data   = Vec::new();
-        reader.read_to_end(&mut data)
-            .expect("Error reading file");
+//         let mut reader = BufReader::new(file);
+//         let mut data   = Vec::new();
+//         reader.read_to_end(&mut data)
+//             .expect("Error reading file");
 
-        let (offset, font) = Font::parse(&data)
-            .expect("Unable to parse font");
+//         let (offset, font) = Font::parse(&data)
+//             .expect("Unable to parse font");
 
-        for table in font.table_iter() {
-            println!("{:?}", table.unwrap());
-        }
+//         for table in font.table_iter() {
+//             println!("{:?}", table.unwrap());
+//         }
 
-        let offset = font.get_table_offset(Tag(*b"hhea"), &offset)
-            .expect("unable to find hhea table!");
+//         let offset = font.get_table_offset(Tag(*b"hhea"), &offset)
+//             .expect("unable to find hhea table!");
 
-        let hhea_buf = &data[offset as usize..];
-        let (_, hhea) = hhea::Hhea::parse(hhea_buf).unwrap();
-        println!("{:?}", hhea);
+//         let hhea_buf = &data[offset as usize..];
+//         let (_, hhea) = hhea::Hhea::parse(hhea_buf).unwrap();
+//         println!("{:?}", hhea);
 
-        println!("{:?}", font);
-    }
-}
+//         println!("{:?}", font);
+//     }
+// }
--- src/util/macros.rs
+++ src/util/macros.rs
@@ -28,3 +28,52 @@ macro_rules! static_size {
         )*
     }
 }
+
+macro_rules! versioned_table {
+    ($name:ty, $i:ty => |$arg:pat| $body:block) => {
+        impl Table for $name {
+            fn parse(buf: &[u8]) -> Result<(&[u8], $name)> {
+                if buf.len() < Self::static_size() {
+                    return Err(Error::UnexpectedEof)
+                }
+
+                let (buf, $arg) = <$i>::parse(buf)?;
+                let res = $body;
+
+                Ok((buf, res))
+            }
+        }
+    }
+}
+
+// versioned_table!(Version,
+//     Tag => |tag| {
+//         const VERSION1: &[u8; 4] = &[0x00, 0x01, 0x00, 0x00];
+//         match &tag.0 {
+//             b"OTTO" => Version::OpenType,
+//             VERSION1 | b"true" | b"typ1" => Version::TrueType,
+//             b"ttcf" => return Err(Error::TtcfUnsupported),
+//             _ => return Err(Error::InvalidData),
+//         }
+//     }
+// );
+
+// impl Table for Version {
+//     fn parse(buf: &[u8]) -> Result<(&[u8], Version)> {
+//         const VERSION1: &[u8; 4] = &[0x00, 0x01, 0x00, 0x00];
+
+//         if buf.len() < Self::static_size() {
+//             return Err(Error::UnexpectedEof)
+//         }
+
+//         let (buf, tag) = Tag::parse(buf)?;
+//         let ver = match &tag.0 {
+//             b"OTTO" => Version::OpenType,
+//             VERSION1 | b"true" | b"typ1" => Version::TrueType,
+//             b"ttcf" => return Err(Error::TtcfUnsupported),
+//             _ => return Err(Error::InvalidData),
+//         };
+
+//         Ok((buf, ver))
+//     }
+// }
